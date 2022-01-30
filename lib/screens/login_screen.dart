ef15cea43192497175201b09fdbfb3fd21baac43
RonnyK36 <ronnykelvyne3@gmail.com>
2022-01-30 22:42:05 +0300

    Login screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/text_input_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +31,38 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
               // logo
-              Container(),
-              Text('Login Page'),
+              SvgPicture.asset(
+                'assets/images/ic_instagram.svg',
+                color: myPrimaryColor,
+                height: 64,
+              ),
+              const SizedBox(height: 60),
+
               // email input
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: "Enter Email",
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               // password field
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Password",
+                isPassword: true,
+                textInputType: TextInputType.visiblePassword,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
               // login button
             ],
           ),
